328397589ee2a4951df28019c18575791d52125b
andre3pazo <61413176+andre3pazo@users.noreply.github.com>
2022-02-04 20:29:54 +0100

    Update line.py

--- line.py
+++ line.py
@@ -12,11 +12,8 @@ def line_intersection(r1, r2):
 
 
 # La funzione verify_intersection verifica se due archi del grafo si incrociano.
-# Se le coordinate del punto di intersezione e dei nodi dell'arco sono molto vicini vengono considerati come lo stesso
-# nodo; infatti a causa della propagazione degli errori si vengono a creare discrepanze.
 def verify_intersection(intersect, node1, node2, node3, node4):
-    if intersect is None or (math.isclose(intersect[0], node1.x) and math.isclose(intersect[1], node1.y)) or \
-            (math.isclose(intersect[0], node2.x) and math.isclose(intersect[1], node2.y)):
+    if intersect is None:
         return False
     if (node1.x > intersect[0] > node2.x) or (node2.x > intersect[0] > node1.x):
         if (node3.x > intersect[0] > node4.x) or (node4.x > intersect[0] > node3.x):
